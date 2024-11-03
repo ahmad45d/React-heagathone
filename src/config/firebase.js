e9a75b5d037c6bf41f99afc29283c9ca82ecac41
ahmad45d <ahmadnetu97@gmail.com>
2024-11-03 19:07:18 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,15 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyARZ00OGddZfyvpo1QdonYcsdY7wlDwTIs",
-  authDomain: "ecomerce-store-41be8.firebaseapp.com",
-  projectId: "ecomerce-store-41be8",
-  storageBucket: "ecomerce-store-41be8.firebasestorage.app",
-  messagingSenderId: "988674781567",
-  appId: "1:988674781567:web:e50fd22b4c680b22c3aa4a",
-  measurementId: "G-6V4QCTYK3D"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
